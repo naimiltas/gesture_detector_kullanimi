a6992c569f43d13e95e6408190502ec4f04f28e7
naimiltas <naimiltas@gmail.com>
2022-09-03 20:02:52 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+                onTap: () {
+                  print("Container tiklandi");
+                },
+                onDoubleTap: () {
+                  print("Container çift tiklandi");
+                },
+                onLongPress: () {
+                  print("Container üzerinde uzun basildi");
+                },
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.red,
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
